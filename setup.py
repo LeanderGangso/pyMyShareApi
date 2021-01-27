1ca9cf6a9ec23a12842a4d8ba8ecd616bf104def
LeanderGangso <leander.gangso98@gmail.com>
2021-01-27 21:51:09 +0100

    setting up setup.py files

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from io import open
+import re
+
+def read(filename):
+    with open(filename, encoding='utf-8') as file:
+        return file.read()
+
+ with open('pma/version.py', 'r', encoding='utf-8') as f:
+    version = re.search(r"^__version__\s*=\s*'(.*)'.*$", f.read(), flags=re.MULTILINE).group(1)
+
+setup(name='pyMyShareAPI',
+        version=version,
+        description='Python MyShare API wrapper',
+        long_description=read('README.md'),
+        long_description_content_type='text/markdown',
+        author='LeanderGangso',
+        author_email='Leander.Gangso98@gmail.com',
+        url='https://github.com/LeanderGangso/pyMyShareAPI',
+        license='GPLv2',
+        keywords='myshare api wrapper',
+        packages=['pymyshareapi'],
+        install_requires=['requests'],
+        extras_require={
+            'json': 'ujson',
+        },
+        classifiers=[
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+            'Operating System :: OS Independent',
+        ],
+)
+
