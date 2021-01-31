e5ef6ffb3981cdd1d93df198d920b3cb83a4bfc9
LeanderGangso <leander.gangso98@gmail.com>
2021-01-31 23:37:52 +0100

    update pypi files

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 from io import open
 import re
 
+base_url = 'https://github.com/LeanderGangso/pyMyShareAPI'
+
 def read(filename):
     with open(filename, encoding='utf-8') as file:
         return file.read()
@@ -10,13 +12,14 @@ with open('mysdk/version.py', 'r', encoding='utf-8') as f:
     version = re.search(r"^__version__\s*=\s*'(.*)'.*$", f.read(), flags=re.MULTILINE).group(1)
 
 setup(name='pyMyShareSDK',
-        version=version,
+        version=version, # gets version from version.py file
         description='Easy MyShare SDK for Python',
         long_description=read('README.md'),
         long_description_content_type='text/markdown',
         author='LeanderGangso',
         author_email='Leander.Gangso98@gmail.com',
-        url='https://github.com/LeanderGangso/pyMyShareAPI',
+        url=base_url,
+        download_url=f'{base_url}/archive/v{version}.tar.gz', # download same version as pypi
         license='LGPLv3',
         keywords='python myshare sdk api tools',
         packages=['mysdk'],
