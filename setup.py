3f3e1e565f65fbb6e06f9228aa5d84bc2fe21f68
LeanderGangso <leander.gangso98@gmail.com>
2021-02-02 12:26:26 +0100

    cleaned up

--- setup.py
+++ setup.py
@@ -1,40 +1,34 @@
 from setuptools import setup
-from io import open
-import re
 
 base_url = 'https://github.com/LeanderGangso/pyMyShareAPI'
 
-requirements = ['requests']
+requirements = [
+    'requests',
+]
 
-def read(filename):
-    with open(filename, encoding='utf-8') as file:
-        return file.read()
+requirements_dev = [
+    'twine==3.3.0',
+    'pytest==6.2.2',
+    'check-manifest==0.46',
+]
 
-with open('mysdk/version.py', 'r', encoding='utf-8') as f:
-    version = re.search(r"^__version__\s*=\s*'(.*)'.*$", f.read(), flags=re.MULTILINE).group(1)
+exec(open('mysdk/version.py', 'r').read()) # get __version__ variable
 
 setup(name='pyMyShareSDK',
-        version=version, # gets version from version.py file
-        description='Easy MyShare SDK for Python',
-        long_description=read('README.md'),
+        version=__version__, # gets version from version.py file
+        description='Python SDK for MyShare API ',
+        long_description=open('README.md', encoding='utf-8').read(),
         long_description_content_type='text/markdown',
         author='LeanderGangso',
         author_email='Leander.Gangso98@gmail.com',
         url=base_url,
-        download_url=f'{base_url}/archive/v{version}.tar.gz', # download from repo release
+        download_url=f'{base_url}/archive/v{__version__}.tar.gz', # download from repo release
         license='MIT',
         keywords='python myshare sdk api tools',
         packages=['mysdk'],
         install_requires=requirements,
         extras_require={
-            'json': 'ujson',
-            'dev': [
-                'twine',
-                'pytest',
-                'check-manifest',
-                'setuptools',
-                'wheel',
-            ]
+            'dev': requirements_dev
         },
         classifiers=[
             'Development Status :: 1 - Planning', # 3 - Alpha, 4 - Beta, 5 - Production/Stable
@@ -42,6 +36,7 @@ setup(name='pyMyShareSDK',
             'Operating System :: OS Independent',
             'Intended Audience :: Developers',
             'Topic :: Software Development :: Libraries :: Python Modules',
+            'Programming Language :: Python',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
